0b035a19cbfaf87f72e79f9051495b6a181ea114
Mansi Seth <81612581+MansiSeth@users.noreply.github.com>
2023-03-27 20:19:44 +0530

    working inventory page with data from json

--- src/components/MyInventoryCard.vue
+++ src/components/MyInventoryCard.vue
@@ -1,80 +1,121 @@
 <template>
-  <div>
-    <v-layout row wrap>
-      <v-flex v-for="inventory in inventory" :key="inventory.i_id">
+  <div class="inventory-card">
+    <img :src="product.image" :alt="product.name" class="product-image" />
 
-        <v-card class="text-xs-center">
+    <h3 class="product-name">{{ product.p_name }}</h3>
 
-          <v-responsive class="pt-4"> image goes here </v-responsive>
-          <v-card-title>{{ inventory.i_name }}</v-card-title>
-      
-        <v-card-text>
-          <div class="subheading">P_ID:{{ inventory.i_id }}</div>
+    <p class="product-id">ID: {{ product.p_id }}</p>
 
-          <div class="grey--text">Price:{{ inventory.i_price }}</div>
-        </v-card-text>
+    <p class="product-price">{{ product.p_price }}</p>
 
-        <v-card-actions>
-          <v-btn flat color="grey">Edit</v-btn>
-        </v-card-actions>
+    <p class="product-description">{{ product.p_desc }}</p>
 
-        </v-card>
-      </v-flex>
-    </v-layout>
-
-    <!-- <v-layout row wrap>
-      <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
-        <v-card flat class="text-xs-center ma-3">
-          <v-responsive class="pt-4"> image here </v-responsive>
-          <v-card-text>
-            <div class="subheading">{{ person.name }}</div>
-            <div class="grey--text">{{ person.role }}</div>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn flat color="grey">
-              <v-icon small left>message</v-icon>
-              <span class="">Message</span>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout> -->
+    <div class="button_pos">
+      <button class="standardButton"
+        @click="
+          $router.push({ name: 'EditProduct', params: { id: product.p_id } })
+        "
+      >Edit</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      inventory: [
-        {
-          i_id: "001",
-          i_name: "MyProduct1",
-          i_price: "100",
-          i_desc: "njgkdsignog",
-        },
-        {
-          i_id: "002",
-          i_name: "MyProduct2",
-          i_price: "100",
-          i_desc: "njgkdsignog",
-        },
-        {
-          i_id: "003",
-          i_name: "MyProduct3",
-          i_price: "100",
-          i_desc: "njgkdsignog",
-        },
-        {
-          i_id: "004",
-          i_name: "MyProduct4",
-          i_price: "100",
-          i_desc: "njgkdsignog",
-        },
-      ],
-    };
+  components: {},
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.inventory-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px 20px 20px 20px;
+  border: 2px solid;
+  border-radius: 30px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  width: 300px;
+  height: 425px;
+  margin: 20px;
+  background-color: #ffe7c2;
+  position: relative;
+}
+
+.product-image {
+  width: 300px;
+  height: 200px;
+  object-fit: cover;
+  margin-bottom: 10px;
+  border-radius: 30px 30px 0px 0px;
+  border-top: 0px solid;
+  border-bottom: 2px solid;
+  border-right: 2px solid;
+  border-left: 2px solid;
+}
+
+.product-name {
+  margin: 0;
+}
+
+.product-id {
+  margin: 0;
+  font-size: 14px;
+  color: #999;
+}
+
+.product-price {
+  margin: 0;
+  font-size: 18px;
+  font-weight: bold;
+  color: #007aff;
+}
+
+.product-description {
+  margin: 10px 0;
+  line-height: 1.5;
+  text-align: justify;
+}
+
+.button_pos {
+  position: absolute;
+  bottom: 10px;
+}
+
+
+.standardButton {
+    background: #FFBD59;
+    background-image: -webkit-linear-gradient(top, #FFBD59, #e6bb7a);
+    background-image: -moz-linear-gradient(top, #FFBD59, #e6bb7a);
+    background-image: -ms-linear-gradient(top, #FFBD59, #e6bb7a);
+    background-image: -o-linear-gradient(top, #FFBD59, #e6bb7a);
+    background-image: linear-gradient(to bottom, #FFBD59, #e6bb7a);
+    -webkit-border-radius: 20;
+    -moz-border-radius: 20;
+    border-radius: 20px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;;
+    color: black;
+    font-size: 13px;
+    padding: 10px 20px 10px 20px;
+    text-decoration: none;
+    border-width: 0;
+    text-align: center;
+    cursor: pointer;
+  }
+  
+  .standardButton:hover {
+    background: #e0aa5a;
+    background-image: -webkit-linear-gradient(top, #e0aa5a, #f5b55a);
+    background-image: -moz-linear-gradient(top, #e0aa5a, #f5b55a);
+    background-image: -ms-linear-gradient(top, #e0aa5a, #f5b55a);
+    background-image: -o-linear-gradient(top, #e0aa5a, #f5b55a);
+    background-image: linear-gradient(to bottom, #e0aa5a, #f5b55a);
+    text-decoration: none;
+  }
+</style>
