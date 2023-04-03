ac964cbf4f0abd2c6123cc936f8fb7a6f32f5692
mansi <81612581+MansiSeth@users.noreply.github.com>
2023-04-03 12:12:18 +0530

    search functionality in inventory

--- src/components/MySearchBar.vue
+++ src/components/MySearchBar.vue
@@ -3,24 +3,20 @@
     
         <div class="pawButton">
     
-            <img class="pawVector" src="@/assets/pawVector.png" alt="paw" />
+            <img class="pawButton" src="@/assets/pawVector.png" alt="paw" />
     
         </div>
     
         <div class="search-bar">
-    
-            <input type="text" placeholder="Search..." />
-    
+
+        <v-text-field solo label="Search" append-icon="mdi-magnify"  v-model="searchText" @input="$emit('search',$event)"></v-text-field>
+
         </div>
-    
-        <div class="search-bar-input">
-    
-            <button type="submit" class="search-btn"><img src="@/assets/search.png"></button></div>
 
 
         <div>
     
-            <button type="submit" class="cart-btn"><img 
+            <button  @click="goToCart" class="cart-btn"><img 
             class="cart-vector" src="@/assets/cart.svg"></button></div>
     
     </div>
@@ -32,6 +28,7 @@ export default {
         return {
             searchTerm: "",
             name: "searchBar",
+            searchText: ''
         };
     },
     methods: {
@@ -39,9 +36,11 @@ export default {
             this.searchTerm = event.target.value;
         },
         search() {
-            // do something with the search term, such as make an API request
-            console.log("Searching for:", this.searchTerm);
+                this.$emit('search', this.searchText);
         },
+         goToCart() {
+    this.$router.push('/cart');
+  }
     },
 };
 </script>
@@ -56,33 +55,25 @@ export default {
   justify-content: center;
   box-shadow: inset 0 0 0 5px #FFBD59; /* add outer border */
 }
-
-
 .pawButton {
-    margin-right: 10px;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin-right: 10px;
 }
-
-.pawVector {
-    width: 30px;
-    border-radius: 30px;
-}
-
-.search-bar {
+/* .search-bar {
     display: flex;
     align-items: center;
     background-color: white;
     border-radius: 10px 0px 0px 10px;
     overflow: hidden;
     font-family: 'Courier New', Courier, monospace;
-}
-
-input[type="text"] {
+}  */
+/* input[type="text"] {
     border: none;
     padding: 5px;
     width: 200px;
-}
-
-
+} */
 .search-btn {
     background-color: #FFBD59;
     border: none;
@@ -91,7 +82,7 @@ input[type="text"] {
     height:25px;
     outline: none;
     border-radius: 0px 15px 15px 0px;;
-}
+} 
 .search-btn:active {
       box-shadow: 0 0 5px rgba(0,0,0,0.5);
       border-radius:5pt;
@@ -100,8 +91,6 @@ input[type="text"] {
 .search-btn :focus{
     outline:none;
 }
-
-
 .search-btn:hover:before {
     transform: scale(1.2);
 }
@@ -109,9 +98,6 @@ input[type="text"] {
     width:30px;
     border-radius: 30%;
 }
-
-
-
 .cart-btn{
     
     width: 30px;
