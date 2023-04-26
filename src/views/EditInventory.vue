6f9ef15888258fcee088a9bdfdda2aef11fed422
mansi <81612581+MansiSeth@users.noreply.github.com>
2023-04-27 02:08:27 +0530

    Edit Inventory Buttons Fix

--- src/views/EditInventory.vue
+++ src/views/EditInventory.vue
@@ -65,7 +65,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .center-container {
   display: flex;
   justify-content: center;
@@ -88,4 +88,33 @@ export default {
     display: flex;
     padding-top: 10%;
 }
+.standardButton {
+    background: #FFBD59;
+    background-image: -webkit-linear-gradient(top, #FFBD59, #e6bb7a);
+    background-image: -moz-linear-gradient(top, #FFBD59, #e6bb7a);
+    background-image: -ms-linear-gradient(top, #FFBD59, #e6bb7a);
+    background-image: -o-linear-gradient(top, #FFBD59, #e6bb7a);
+    background-image: linear-gradient(to bottom, #FFBD59, #e6bb7a);
+    -webkit-border-radius: 20;
+    -moz-border-radius: 20;
+    border-radius: 20px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;;
+    color: black;
+    font-size: 13px;
+    padding: 10px 20px 10px 20px;
+    text-decoration: none;
+    border-width: 0;
+    text-align: center;
+    cursor: pointer;
+  }
+  
+  .standardButton:hover {
+    background: #e0aa5a;
+    background-image: -webkit-linear-gradient(top, #e0aa5a, #f5b55a);
+    background-image: -moz-linear-gradient(top, #e0aa5a, #f5b55a);
+    background-image: -ms-linear-gradient(top, #e0aa5a, #f5b55a);
+    background-image: -o-linear-gradient(top, #e0aa5a, #f5b55a);
+    background-image: linear-gradient(to bottom, #e0aa5a, #f5b55a);
+    text-decoration: none;
+  }
 </style>
